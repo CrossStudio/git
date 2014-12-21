ca95ab43e7a402b50556437f32bb321173b7781c
xamcross <xamcross@gmail.com>
2014-12-21 14:02:42 +0200

    Привел класс Suit к окончательному виду

--- Durak/src/com/example/durak/Suit.java
+++ Durak/src/com/example/durak/Suit.java
@@ -1,21 +1,28 @@
 package com.example.durak;
 
-
 public enum Suit {
-	SPADES(0), CLUBS(1), DIAMONDS(2), HEARTS(3);
+	SPADES(0, "Spades"), CLUBS(1, "Clubs"), DIAMONDS(2, "Diamonds"), HEARTS(3, "Hearts");
 	
 	private int numValue;
+	private String stringValue;
 	
-	Suit(int numValue){
+	public static final int size = Suit.values().length;
+	
+	Suit(int numValue, String stringValue){
 		this.numValue = numValue;
+		this.stringValue = stringValue;
 	}
 	
 	public int getNumValue(){
 		return numValue;
 	}
 	
+	public String getStringValue(){
+		return stringValue;
+	}
+	
 	public String toString(){
-		return "" + numValue;
+		return stringValue;
 	}
 	
 	
