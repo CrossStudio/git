19ba4a39b2258754d9a4e55bfead32e2db8f8028
xamcross <xamcross@gmail.com>
2014-12-21 14:00:19 +0200

    Привел класс CardValue к окончаетельному виду

--- Durak/src/com/example/durak/CardValue.java
+++ Durak/src/com/example/durak/CardValue.java
@@ -1,20 +1,26 @@
 package com.example.durak;
 
-
 public enum CardValue {
-	SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(14);
+	SIX(6, "6"), SEVEN(7, "7"), EIGHT(8, "8"), NINE(9, "9"), TEN(10, "10"), JACK(11, "Jack"),
+	QUEEN(12, "Queen"), KING(13, "King"), ACE(14, "Ace");
 	
 	private int numVal;
+	private String stringVal;
 	
-	CardValue(int numVal){
+	CardValue(int numVal, String stringVal){
 		this.numVal = numVal;
+		this.stringVal = stringVal;
 	}
 	
 	public int getNumValue(){
 		return numVal;
 	}
 	
+	public String getStringValue(){
+		return stringVal;
+	}
+	
 	public String toString(){
-		return "" + numVal;
+		return stringVal;
 	}
 }
