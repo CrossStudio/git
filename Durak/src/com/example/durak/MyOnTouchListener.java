cf29ff880518844111f2d6423bc3d49cbbae2ad3
xamcross <xamcross@gmail.com>
2015-01-22 11:38:08 +0200

    Revert changes

--- Durak/src/com/example/durak/MyOnTouchListener.java
+++ Durak/src/com/example/durak/MyOnTouchListener.java
@@ -12,17 +12,53 @@ public class MyOnTouchListener implements OnTouchListener {
 
 	private ArrayList<Object> dataToSend = new ArrayList<Object>();
 	
+	Card cardPlayed;
+	View cardView;
+	
 	public MyOnTouchListener(Card cardPlayed, View cardView) {
+		this.cardPlayed = cardPlayed;
+		this.cardView = cardView;
 		dataToSend.add(cardPlayed);
 		dataToSend.add(cardView);
 	}
 	
 	public boolean onTouch(View view, MotionEvent motionEvent) {
-		System.out.println("Moving a card");
-		System.out.println("Card touched: " + dataToSend.get(0));
-		ClipData data = ClipData.newPlainText(" ", " ");
+		//If this is a move as an attacking player
+		if (!GameActivity.getInstance().getHumanPlayer().isDefender()){
+			System.out.println("Human player attacks");
+			//If this move is the last chance to attack the defender who is overwhelmed
+			if (GameActivity.getDefendingPlayer().isOverwhelmed()){
+				if (GameActivity.getInstance().humanPlayerAttack(cardPlayed)){
+					UIOperator.getInstance().UIDrawNewAttackCard(cardPlayed);
+				}
+			}
+			//If this is a general attacking move
+			else {
+				if (GameActivity.getInstance().humanPlayerAttack(cardPlayed)){
+					UIOperator.getInstance().UIDrawNewAttackCard(cardPlayed);
+					GameActivity.getInstance().letDefenderMove();
+				}
+			}
+			GameActivity.getInstance().btnEndMove.setEnabled(true);
+		}
+		//If this is a move as a defending player
+		else {
+			System.out.println("Human player defends");
+			if (GameActivity.getInstance().humanPlayerDefend(cardPlayed, Table.getUnbeatenCards().get(0))){
+				if (GameActivity.getInstance().getHumanPlayer().getCardsOnHand().isEmpty()){
+					GameActivity.getInstance().endTurn();
+					return true;
+				}
+				GameActivity.currentPlayerIndex = -1;
+				GameActivity.getInstance().letNextAttackerMove();
+			}
+			else {
+				System.out.println("You cannot defend " + Table.getUnbeatenCards().get(0) + " with "+ cardPlayed);
+			}
+		}
+		/*ClipData data = ClipData.newPlainText(" ", " ");
 	    DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
-	    view.startDrag(data, shadowBuilder, dataToSend, 0);
+	    view.startDrag(data, shadowBuilder, dataToSend, 0);*/
 		return false;
 	}
 }
