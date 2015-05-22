0021d050bd8e53105e99e41ab70a47c0d973c33e
xamcross <xamcross@gmail.com>
2015-05-22 12:18:00 +0300

    Changed the NextCharacterListener listing and added some comments to it

--- DNDDungeonMasterHelper/src/dnd/dungeon_master_helper/NextCharacterClickListener.java
+++ DNDDungeonMasterHelper/src/dnd/dungeon_master_helper/NextCharacterClickListener.java
@@ -1,30 +1,50 @@
 package dnd.dungeon_master_helper;
 
+import java.util.ArrayList;
+
 import android.view.View;
 import android.view.View.OnClickListener;
 
+/**
+ * Uses OnClickListener interface to make Next Character button of the main interface function
+ * @author XAM
+ *
+ */
 public class NextCharacterClickListener implements OnClickListener {
 
+	ArrayList<DNDCharacter> charactersList = MainActivity.dndCharacterArrayList;
+	DNDCharacter activeCharacter = MainActivity.activeCharacter;
+	
 	@Override
 	public void onClick(View v) {
-		for (DNDCharacter character : MainActivity.dndCharacterArrayList)
+		for (DNDCharacter character : charactersList)
 		{
-			if (character.equals(MainActivity.activeCharacter))
+			if (character.equals(activeCharacter))
 			{
-				if (MainActivity.dndCharacterArrayList.indexOf(character) < MainActivity.dndCharacterArrayList.size() - 1)
-				{
-					MainActivity.activeCharacter = MainActivity.dndCharacterArrayList.
-							get(MainActivity.dndCharacterArrayList.indexOf(character) + 1);
-				}
-				else
-				{
-					MainActivity.activeCharacter = MainActivity.dndCharacterArrayList.get(0);
-				}
-				MainActivity.tvActiveCharacter.setText("Active Character: " + MainActivity.activeCharacter.getCharName());
+				nextCharacter(activeCharacter);
 				break;
 			}
 		}
 		
+	}
+	
+	/**
+	 * Sets currently active character equal to the next character with regards to the character sent to the method
+	 * Also changes text in the "Active Character" TextView
+	 * @param activeCharacter - currently active character
+	 */
+	private void nextCharacter(DNDCharacter activeCharacter)
+	{
+		if (charactersList.indexOf(activeCharacter) < charactersList.size() - 1)
+		{
+			activeCharacter = charactersList.
+					get(charactersList.indexOf(activeCharacter) + 1);
+		}
+		else
+		{
+			activeCharacter = charactersList.get(0);
+		}
+		MainActivity.tvActiveCharacter.setText("Active Character: " + activeCharacter.getCharName());
 	}
 
 
