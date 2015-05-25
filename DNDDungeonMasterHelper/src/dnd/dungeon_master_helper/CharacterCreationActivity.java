311dabe34b252ed5fdb13e3ac07e985ce5fa9c38
xamcross <xamcross@gmail.com>
2015-05-25 16:22:42 +0300

    Added new layout for adding new character Added several listeners for the new activity's UI (still have to implement them)

--- DNDDungeonMasterHelper/src/dnd/dungeon_master_helper/CharacterCreationActivity.java
+++ DNDDungeonMasterHelper/src/dnd/dungeon_master_helper/CharacterCreationActivity.java
@@ -1,7 +1,38 @@
 package dnd.dungeon_master_helper;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CharacterCreationActivity extends Activity {
 
+	Button btnAddNewCharacter;
+	Button btnProceedToGame;
+	EditText etCharName;
+	Spinner spCharClass;
+	
+	@Override
+	public void onCreate(Bundle savedBundle){
+		super.onCreate(savedBundle);
+		setContentView(R.layout.character_creation);
+		
+		initializeViews();
+	}
+	
+	/**
+	 * Assign initial values to various View variables along with listeners
+	 */
+	private void initializeViews() {
+		btnAddNewCharacter = (Button) findViewById(R.id.btnAddNewCharacter);
+		btnProceedToGame = (Button) findViewById(R.id.btnProceedToGame);
+		etCharName = (EditText) findViewById(R.id.etCharName);
+		spCharClass = (Spinner) findViewById(R.id.spCharClass);
+		
+		btnAddNewCharacter.setOnClickListener(new AddNewCharacterListener());
+		btnProceedToGame.setOnClickListener(new ProceedToGameListener());
+		
+		spCharClass.setOnItemSelectedListener(new CharClassSelectedListener());
+	}
 }
---
+++ DNDDungeonMasterHelper/src/dnd/dungeon_master_helper/ProceedToGameListener.java
@@ -0,0 +1,13 @@
+package dnd.dungeon_master_helper;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ProceedToGameListener implements OnClickListener {
+
+	@Override
+	public void onClick(View v) {
+
+	}
+
+}
